2604611c358e32fbef4567a6da29f239ae678afc
Fernando Almeida <nando.lalmeida@gmail.com>
2022-05-11 14:52:31 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes.js'));
 
 module.exports = router;
