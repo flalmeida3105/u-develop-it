d074e77da0a1f80744b5d93354258e73e48346ca
Fernando Almeida <nando.lalmeida@gmail.com>
2022-05-10 23:55:18 -0400

    assigned a foreing key to candidates table

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ db.connect((err) => {
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `
+        SELECT candidates.*, parties.name 
+        AS party_name 
+        FROM candidates 
+        LEFT JOIN parties 
+        ON candidates.party_id = parties.id
+        `;
     
     db.query(sql, (err, rows) => {
         if (err) {
@@ -41,8 +47,16 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `
+        SELECT candidates.*, parties.name 
+        AS party_name 
+        FROM candidates 
+        LEFT JOIN parties 
+        ON candidates.party_id = parties.id 
+        WHERE candidates.id = ?
+        `;
     const params = [req.params.id];
+    
     db.query(sql, params, (err, rows) => {
         if (err) {
             res.status(400).json({error: err.message});
