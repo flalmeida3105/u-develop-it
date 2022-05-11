9a5287b86f4c6569975bb36bb176be40c96344f6
Fernando Almeida <nando.lalmeida@gmail.com>
2022-05-10 21:34:20 -0400

    added db queries to test create/delete/update actions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const { nextTick } = require("process");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -22,19 +23,41 @@ db.connect((err) => {
     console.log('Connected to MySQL Server!');
 });
 
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 0`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// // Delete a candidate
+// // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// //     if (err) {
+// //         console.log(err);
+// //     }
+// //     console.log(result);
+// // });
+
+// // Pre-requisites for creating new candidate
+// // define sql query
+// const sql = `INSERT INTO candidates(id, first_name, last_name, industry_connected) VALUES(?,?,?,?)`;
+// // define sql parameters 
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // creating candidate
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result)
+// });
+
+// // Setting a default response if the request it's not supported by the app
+// app.use((req, res) => {
+//     res.status(404).end();
+// })
+
 // starting Express Server on Port 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-// Testing route
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello'
-//     });
-// })
-
-// Setting a default response if the request it's not supported by the app
-app.use((req, res) => {
-    res.status(404).end();
-})
