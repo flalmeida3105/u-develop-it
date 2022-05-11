d940d6be46cd9084ff07d6d3f1c30ea367f0442f
Fernando Almeida <nando.lalmeida@gmail.com>
2022-05-11 18:24:46 -0400

    added votes count and votes post

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes.js'));
+router.use(require('./voteRoutes.js'));
 
 module.exports = router;
